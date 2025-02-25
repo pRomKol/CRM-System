3cc0be704c6cd7acdfea0e0a85f620008ac7aa14
Roman <sselbstmord@gmail.com>
2025-02-25 11:08:04 +0300

    q

--- src/features/todo.api.ts
+++ src/features/todo.api.ts
@@ -2,28 +2,22 @@ import axios from 'axios';
 
 const baseURL = 'https://easydev.club/api/v1';
 
-export async function addTodo(todoRequest) {
+export async function addTodo(todoRequest: any): Promise<any> {
     const response = await axios.post(`${baseURL}/todos`, todoRequest);
     return response.data;
 }
 
-export async function updateTodo(id, todoRequest) {
+export async function updateTodo(id: number, todoRequest: any):Promise<any>  {
     const response = await axios.put(`${baseURL}/todos/${id}`, todoRequest);
     return response.data;
 }
 
-export async function deleteTodo(id) {
+export async function deleteTodo(id: number):Promise<any> {
     const response = await axios.delete(`${baseURL}/todos/${id}`);
     console.log('зпрос на удаление')
     return response.data;
 }
-
-export async function getTodoById(id) {
-    const response = await axios.get(`${baseURL}/todos/${id}`);
-    return response.data;
-}
-
-export async function getTodos(filter = 'all') {
+export async function getTodos(filter = 'all'):Promise<any> {
     const response = await axios.get(`${baseURL}/todos`, { params: { filter } });
     return response.data;
 }
